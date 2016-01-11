fa8b376747394a8c23f8477e7373e724069f053d
Marc-Andre Laperle <malaperle@gmail.com>
2016-01-11 16:09:23 -0500

    PLUG9_Add polling job that prints to the console

--- org.eclipse.cdt.example.framespy/src/org/eclipse/cdt/example/framespy/FrameSpyView.java
+++ org.eclipse.cdt.example.framespy/src/org/eclipse/cdt/example/framespy/FrameSpyView.java
@@ -9,6 +9,10 @@
 
 package org.eclipse.cdt.example.framespy;
 
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.core.runtime.jobs.Job;
 import org.eclipse.core.runtime.preferences.IEclipsePreferences;
 import org.eclipse.core.runtime.preferences.InstanceScope;
 import org.eclipse.jface.action.MenuManager;
@@ -23,8 +27,9 @@ public class FrameSpyView extends ViewPart {
 
 	private static final String TOGGLE_STATE_PREF_KEY = "toggle.state";
 	private MenuManager fMenuManager;
-	private boolean fToggledState;
+//	private boolean fToggledState;
 	private Label fToggledStateLbl;
+private Job fPollingJob;
 
 	public FrameSpyView() {
 		// TODO Auto-generated constructor stub
@@ -40,10 +45,13 @@ public class FrameSpyView extends ViewPart {
 		getViewSite().registerContextMenu(fMenuManager, null);
 		
 		fToggledStateLbl = new Label(composite, SWT.NONE);
-
-		IEclipsePreferences preferences = InstanceScope.INSTANCE.getNode(Activator.PLUGIN_ID);
-		String togglePrefValue = preferences.get(TOGGLE_STATE_PREF_KEY, Boolean.FALSE.toString());
-		fToggledStateLbl.setText(togglePrefValue);
+		// Display the new state to the user
+		boolean toggledState = getToggledState();
+		fToggledStateLbl.setText(Boolean.toString(toggledState));
+		// Create the polling job if the spy is enabled
+		if (toggledState) {
+			startPollingJob();
+		}
 	}
 
 	@Override
@@ -59,15 +67,56 @@ public class FrameSpyView extends ViewPart {
 	}
 
 	public boolean getToggledState() {
-		return fToggledState;
-	}
-
-	public void setToggledState(boolean toggledState) {
-		fToggledState = toggledState;
-		fToggledStateLbl.setText(Boolean.toString(fToggledState));
-		// Save the toggle state in a preference so that it's remembered next time the view is opened
 		IEclipsePreferences preferences = InstanceScope.INSTANCE.getNode(Activator.PLUGIN_ID);
-		preferences.put(TOGGLE_STATE_PREF_KEY, Boolean.toString(fToggledState));
+		String togglePrefValue = preferences.get(TOGGLE_STATE_PREF_KEY, Boolean.FALSE.toString());
+		return Boolean.parseBoolean(togglePrefValue);
 	}
 
+	public void setToggledState(boolean newState) {
+		boolean oldState = getToggledState();
+		if (oldState != newState) {
+			// Display the new state to the user
+			fToggledStateLbl.setText(Boolean.toString(newState));
+
+			// Save the toggle state in a preference so that it's remembered
+			// next time the view is opened
+			IEclipsePreferences preferences = InstanceScope.INSTANCE.getNode(Activator.PLUGIN_ID);
+			preferences.put(TOGGLE_STATE_PREF_KEY, Boolean.toString(newState));
+						
+			// Create the polling job if the spy is enabled
+			if (newState) {
+				startPollingJob();
+			} else {
+				cancelPollingJob();
+			}
+		}
+
+	}
+
+	private void startPollingJob() {
+		fPollingJob = new Job("Frame Spy Polling Job") {
+
+			@Override
+			protected IStatus run(IProgressMonitor monitor) {
+				try {
+					Thread.sleep(1000);
+				} catch (InterruptedException e) {
+					// Ignored
+				}
+				System.out.println("Polling");
+				if (getToggledState()) {
+					schedule();
+				}
+				return Status.OK_STATUS;
+			}
+
+		};
+		fPollingJob.schedule();
+	}
+
+	private void cancelPollingJob() {
+		if (fPollingJob != null) {
+			fPollingJob.cancel();
+		}
+	}
 }
