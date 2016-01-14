d58edf29e9042a8da5a20fb9488d71765796bd29
Marc-Andre Laperle <malaperle@gmail.com>
2016-01-14 18:40:11 -0500

    CODAN1.4_Create and report problem

--- org.eclipse.cdt.example.codeanalysis/src/org/eclipse/cdt/example/codeanalysis/LineCountChecker.java
+++ org.eclipse.cdt.example.codeanalysis/src/org/eclipse/cdt/example/codeanalysis/LineCountChecker.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 import org.eclipse.cdt.codan.core.model.AbstractCheckerWithProblemPreferences;
+import org.eclipse.cdt.codan.core.model.IProblemWorkingCopy;
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.runtime.CoreException;
@@ -12,6 +13,8 @@ import org.eclipse.core.runtime.OperationCanceledException;
 
 public class LineCountChecker extends AbstractCheckerWithProblemPreferences {
 
+	final static String FILE_TOO_LONG_PROBLEM_ID = "org.eclipse.cdt.example.codeanalysis.toolong";
+	
 	@Override
 	public boolean processResource(IResource resource) throws OperationCanceledException {
 		if (!shouldProduceProblems(resource)) {
@@ -34,11 +37,27 @@ public class LineCountChecker extends AbstractCheckerWithProblemPreferences {
 			while ((line = bis.readLine()) != null) {
 				numLines++;
 			}
-			System.out.println(file.getName() + " " + numLines);
+			
+			if (numLines > 100) {
+				reportProblem(FILE_TOO_LONG_PROBLEM_ID, file, 1);
+			}
 		} catch (IOException e) {
 			// ignore
 		} catch (CoreException e) {
 			// ignore
 		}
+	}
+	
+	private int getMaxLineCount(IFile file) {
+		//TODO: Call getProblemById to get the problem
+		//TODO: Call getPreference to get a given preference for that problem
+		int maxLineCount = 0;
+		return 0;
+	}
+
+	@Override
+	public void initPreferences(IProblemWorkingCopy problem) {
+		super.initPreferences(problem);
+		//TODO: Call addPreference(IProblemWorkingCopy, String, ...) to let the framework and UI be aware of the preference
 	}
 }
