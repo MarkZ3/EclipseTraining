647ad00483b4e11831b6c23423226b51e25ae0e4
Marc Khouzam <marc.khouzam@ericsson.com>
2016-01-15 09:23:19 -0500

    Add dependencies and store BundleContext

--- org.eclipse.cdt.example.framespy/src/org/eclipse/cdt/example/framespy/Activator.java
+++ org.eclipse.cdt.example.framespy/src/org/eclipse/cdt/example/framespy/Activator.java
@@ -22,38 +22,32 @@ public class Activator extends AbstractUIPlugin {
 
 	// The shared instance
 	private static Activator plugin;
+	private static BundleContext bundleContext;
 	
-	/**
-	 * The constructor
-	 */
 	public Activator() {
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext)
-	 */
 	public void start(BundleContext context) throws Exception {
 		super.start(context);
 		plugin = this;
+		bundleContext = context;
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext)
-	 */
 	public void stop(BundleContext context) throws Exception {
 		plugin = null;
 		super.stop(context);
+		bundleContext = null;
 	}
 
 	/**
 	 * Returns the shared instance
-	 *
-	 * @return the shared instance
 	 */
 	public static Activator getDefault() {
 		return plugin;
 	}
+
+    public static BundleContext getBundleContext() {
+        return bundleContext;
+    }
 
 }
