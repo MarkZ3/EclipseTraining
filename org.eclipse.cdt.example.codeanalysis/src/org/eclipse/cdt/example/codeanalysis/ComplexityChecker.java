d55adfebe403b49f2dd27396ba10e078d6f00bea
Marc-Andre Laperle <malaperle@gmail.com>
2016-01-16 16:54:38 -0500

    CODAN2.2_Create a visitor that visits statements

--- org.eclipse.cdt.example.codeanalysis/src/org/eclipse/cdt/example/codeanalysis/ComplexityChecker.java
+++ org.eclipse.cdt.example.codeanalysis/src/org/eclipse/cdt/example/codeanalysis/ComplexityChecker.java
@@ -1,13 +1,34 @@
 package org.eclipse.cdt.example.codeanalysis;
 
 import org.eclipse.cdt.codan.core.cxx.model.AbstractIndexAstChecker;
+import org.eclipse.cdt.core.dom.ast.ASTVisitor;
+import org.eclipse.cdt.core.dom.ast.IASTStatement;
 import org.eclipse.cdt.core.dom.ast.IASTTranslationUnit;
 
 public class ComplexityChecker extends AbstractIndexAstChecker {
 
+	private final class ComplexityVisitor extends ASTVisitor {
+		
+		{
+			shouldVisitStatements = true;
+		}
+		
+		@Override
+		public int visit(IASTStatement statement) {
+			// TODO: increment complexity level
+			return super.visit(statement);
+		}
+		
+		@Override
+		public int leave(IASTStatement statement) {
+			// TODO: decrement complexity level
+			return super.leave(statement);
+		}
+	}
+
 	@Override
 	public void processAst(IASTTranslationUnit ast) {
-		// TODO: Create and use ASTVisitor here (either an anonymous class or a separate nested class somewhere in the file
+		ast.accept(new ComplexityVisitor());
 	}
 
 }
