c8e12652dbf00d12d66bbe467bdfbbfa35e0581c
Marc-Andre Laperle <malaperle@gmail.com>
2016-01-16 16:44:12 -0500

    CODAN2.1_ComplexityChecker created with nothing implemented

---
+++ org.eclipse.cdt.example.codeanalysis/src/org/eclipse/cdt/example/codeanalysis/ComplexityChecker.java
@@ -0,0 +1,13 @@
+package org.eclipse.cdt.example.codeanalysis;
+
+import org.eclipse.cdt.codan.core.cxx.model.AbstractIndexAstChecker;
+import org.eclipse.cdt.core.dom.ast.IASTTranslationUnit;
+
+public class ComplexityChecker extends AbstractIndexAstChecker {
+
+	@Override
+	public void processAst(IASTTranslationUnit ast) {
+		// TODO: Create and use ASTVisitor here (either an anonymous class or a separate nested class somewhere in the file
+	}
+
+}
