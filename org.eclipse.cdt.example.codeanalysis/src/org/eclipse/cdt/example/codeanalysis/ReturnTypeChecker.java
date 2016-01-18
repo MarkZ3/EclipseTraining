8898760493cfd6784f76f0e46be7dfdf85c3139d
Marc-Andre Laperle <malaperle@gmail.com>
2016-01-17 23:44:45 -0500

    CODAN3.1_ReturnTypeChecker created with nothing implemented

---
+++ org.eclipse.cdt.example.codeanalysis/src/org/eclipse/cdt/example/codeanalysis/ReturnTypeChecker.java
@@ -0,0 +1,16 @@
+package org.eclipse.cdt.example.codeanalysis;
+
+import org.eclipse.cdt.codan.core.cxx.model.AbstractIndexAstChecker;
+import org.eclipse.cdt.core.dom.ast.IASTTranslationUnit;
+
+public class ReturnTypeChecker extends AbstractIndexAstChecker {
+
+	public ReturnTypeChecker() {
+	}
+
+	@Override
+	public void processAst(IASTTranslationUnit ast) {
+		// TODO: Create and use ASTVisitor here (either an anonymous class or a separate nested class somewhere in the file
+	}
+
+}
