b3f970108473ed6d498dbc16f1a8b4e60c94e9e2
Marc Khouzam <marc.khouzam@ericsson.com>
2016-01-18 16:05:36 -0500

    Register new service

--- org.eclipse.cdt.example.framespy/src/org/eclipse/cdt/example/framespy/FrameSpyService.java
+++ org.eclipse.cdt.example.framespy/src/org/eclipse/cdt/example/framespy/FrameSpyService.java
@@ -9,6 +9,9 @@
 
 package org.eclipse.cdt.example.framespy;
 
+import java.util.Hashtable;
+
+import org.eclipse.cdt.dsf.concurrent.RequestMonitor;
 import org.eclipse.cdt.dsf.service.AbstractDsfService;
 import org.eclipse.cdt.dsf.service.DsfSession;
 import org.osgi.framework.BundleContext;
@@ -22,5 +25,25 @@ public class FrameSpyService extends AbstractDsfService {
 	@Override
 	protected BundleContext getBundleContext() {
 		return Activator.getBundleContext();
+	}
+	
+	@Override
+	public void initialize(RequestMonitor rm) {
+		super.initialize(
+			new RequestMonitor(getExecutor(), rm) {
+				@Override
+				protected void handleSuccess() {
+					// Register when the service is ready to be called
+					register(new String[]{FrameSpyService.class.getName()}, new Hashtable<String,String>());
+					rm.done();
+				}
+			});
+	}
+	
+	@Override
+	public void shutdown(RequestMonitor rm) {
+		// Unregister first, so that no one calls us anymore
+		unregister();
+		super.shutdown(rm);
 	}
 }
