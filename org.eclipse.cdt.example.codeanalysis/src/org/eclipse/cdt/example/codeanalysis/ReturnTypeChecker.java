d37280d508bd82ee77d342f882c2ccb61f45c848
Marc-Andre Laperle <malaperle@gmail.com>
2016-01-18 00:14:36 -0500

    Create a visitor that visits declarators

--- org.eclipse.cdt.example.codeanalysis/src/org/eclipse/cdt/example/codeanalysis/ReturnTypeChecker.java
+++ org.eclipse.cdt.example.codeanalysis/src/org/eclipse/cdt/example/codeanalysis/ReturnTypeChecker.java
@@ -1,16 +1,30 @@
 package org.eclipse.cdt.example.codeanalysis;
 
 import org.eclipse.cdt.codan.core.cxx.model.AbstractIndexAstChecker;
+import org.eclipse.cdt.core.dom.ast.ASTVisitor;
+import org.eclipse.cdt.core.dom.ast.IASTDeclarator;
 import org.eclipse.cdt.core.dom.ast.IASTTranslationUnit;
 
 public class ReturnTypeChecker extends AbstractIndexAstChecker {
+
+	private final class MethodDeclaratorVisitor extends ASTVisitor {
+
+		{
+			shouldVisitDeclarators = true;
+		}
+
+		@Override
+		public int visit(IASTDeclarator declarator) {
+			return super.visit(declarator);
+		}
+		
+	}
 
 	public ReturnTypeChecker() {
 	}
 
 	@Override
 	public void processAst(IASTTranslationUnit ast) {
-		// TODO: Create and use ASTVisitor here (either an anonymous class or a separate nested class somewhere in the file
+		ast.accept(new MethodDeclaratorVisitor());
 	}
-
 }
