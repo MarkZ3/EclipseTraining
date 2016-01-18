bc70177502c97b66ca331f08ba0c3fb06cd127d0
Marc-Andre Laperle <malaperle@gmail.com>
2016-01-18 00:20:58 -0500

    CODAN3.2_Obtain a ICPPMethod, call checkConflictingReturn

--- org.eclipse.cdt.example.codeanalysis/src/org/eclipse/cdt/example/codeanalysis/ReturnTypeChecker.java
+++ org.eclipse.cdt.example.codeanalysis/src/org/eclipse/cdt/example/codeanalysis/ReturnTypeChecker.java
@@ -4,6 +4,11 @@ import org.eclipse.cdt.codan.core.cxx.model.AbstractIndexAstChecker;
 import org.eclipse.cdt.core.dom.ast.ASTVisitor;
 import org.eclipse.cdt.core.dom.ast.IASTDeclarator;
 import org.eclipse.cdt.core.dom.ast.IASTTranslationUnit;
+import org.eclipse.cdt.core.dom.ast.IBinding;
+import org.eclipse.cdt.core.dom.ast.cpp.ICPPClassType;
+import org.eclipse.cdt.core.dom.ast.cpp.ICPPMethod;
+import org.eclipse.cdt.core.parser.util.ArrayUtil;
+import org.eclipse.cdt.internal.core.dom.parser.cpp.ClassTypeHelper;
 
 public class ReturnTypeChecker extends AbstractIndexAstChecker {
 
@@ -15,9 +20,38 @@ public class ReturnTypeChecker extends AbstractIndexAstChecker {
 
 		@Override
 		public int visit(IASTDeclarator declarator) {
+			IBinding binding = declarator.getName().resolveBinding();
+			if (binding instanceof ICPPMethod) {
+				checkConflictingReturn((ICPPMethod) binding, declarator);
+			}
 			return super.visit(declarator);
 		}
-		
+
+		private void checkConflictingReturn(ICPPMethod methodChecked, IASTDeclarator declarator) {
+			// TODO: Get the class type of this method. i.e. the 'owner'
+			// TODO: Get all the base methods of this class using getAllBaseMethods
+			
+			// TODO: for each method
+			//    - if the method is virtual
+			//    - check if the name (getName) is equal to the methodChecked name
+			//    - check if the return type is not the same (so that it can be reported
+ 
+		}
+
+		/**
+		 * Return all base methods of a given class type
+		 * 
+		 * @param classType the class type
+		 * @return the base methods
+		 */
+		private ICPPMethod[] getAllBaseMethods(ICPPClassType classType) {
+			ICPPClassType[] bases = ClassTypeHelper.getAllBases(classType, null);
+			ICPPMethod[] methods = null;
+			for (ICPPClassType base : bases) {
+				methods = ArrayUtil.addAll(ICPPMethod.class, methods, ClassTypeHelper.getDeclaredMethods(base, null));
+			}
+			return methods;
+		}
 	}
 
 	public ReturnTypeChecker() {
