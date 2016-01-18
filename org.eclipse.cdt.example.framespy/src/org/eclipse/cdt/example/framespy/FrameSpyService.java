b2913893da6b787064467a34885cefb4cc46e6a6
Marc Khouzam <marc.khouzam@ericsson.com>
2016-01-18 16:11:05 -0500

    DSF4_ANSWERS: Implement getTimeOfDayString() method in new service

--- org.eclipse.cdt.example.framespy/src/org/eclipse/cdt/example/framespy/FrameSpyService.java
+++ org.eclipse.cdt.example.framespy/src/org/eclipse/cdt/example/framespy/FrameSpyService.java
@@ -9,8 +9,11 @@
 
 package org.eclipse.cdt.example.framespy;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Hashtable;
 
+import org.eclipse.cdt.dsf.concurrent.DataRequestMonitor;
 import org.eclipse.cdt.dsf.concurrent.RequestMonitor;
 import org.eclipse.cdt.dsf.service.AbstractDsfService;
 import org.eclipse.cdt.dsf.service.DsfSession;
@@ -45,5 +48,17 @@ public class FrameSpyService extends AbstractDsfService {
 		// Unregister first, so that no one calls us anymore
 		unregister();
 		super.shutdown(rm);
+	}
+	
+	public String getLocalTimeOfDayString() {
+		return new SimpleDateFormat("HH:mm:ss").format(Calendar.getInstance().getTime());
+	}
+	
+	// The asynchronous method below is more versatile because it could
+	// be used to go ask GDB the time of day on the target.
+	// Since sending message to GDB is considered long, we must use an
+	// asynchronous method.
+	public void getTargetTimeOfDayString(DataRequestMonitor<String> rm) {
+		rm.done(new SimpleDateFormat("HH:mm:ss").format(Calendar.getInstance().getTime()));
 	}
 }
