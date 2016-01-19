829acb8f82584a566650d37f04730e4540e741bf
Marc Khouzam <marc.khouzam@ericsson.com>
2016-01-19 13:24:13 -0500

    DSF7.1_ADVANCED: Advanced instructions

---
+++ org.eclipse.cdt.example.framespy/src/org/eclipse/cdt/example/framespy/FrameSpyStackService.java
@@ -0,0 +1,24 @@
+/*******************************************************************************
+ * Copyright (c) 2016 Ericsson
+ *
+ * All rights reserved. This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License v1.0 which
+ * accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+
+package org.eclipse.cdt.example.framespy;
+
+public class FrameSpyStackService {
+
+	// Global TODO: Replace the "main" function name with "entry" 
+	
+	// TODO: Convert this file into an extension of DSF-GDB's Stack service.
+	
+	// TODO: Override getFrameData() to return a different IFrameDMData,
+	//       in which any "main" method is called "entry"
+	
+	// Note that this part of the exercise cannot be tested just yet.  It needs
+	// more changes which we will do as follow-ups.
+
+}
