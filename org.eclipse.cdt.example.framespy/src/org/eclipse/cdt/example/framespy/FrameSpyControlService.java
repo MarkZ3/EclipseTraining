9c063163536830b657fcd0b9114de777675fd9a3
Marc Khouzam <marc.khouzam@ericsson.com>
2016-01-20 10:14:13 -0500

    DSF9.3_ADVANCED: Advanced instructions to override control service

---
+++ org.eclipse.cdt.example.framespy/src/org/eclipse/cdt/example/framespy/FrameSpyControlService.java
@@ -0,0 +1,19 @@
+/*******************************************************************************
+ * Copyright (c) 2016 Ericsson
+ *
+ * All rights reserved. This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License v1.0 which
+ * accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+
+package org.eclipse.cdt.example.framespy;
+
+public class FrameSpyControlService {
+
+	// Global TODO: Extend the latest ICommandControlService to instantiate the new
+	//              FrameSpyFinalLaunchSequence (hint: look at how FinalLaunchSequence_7_7 is instantiated).
+	//
+	//              Finally, instantiate the new FrameSpyControlService as we did for our other
+	//              overridden service.
+}
