e3a884ba4dd2148ddbba713febb6160dfd7fa15e
Marc Khouzam <marc.khouzam@ericsson.com>
2016-01-20 10:12:24 -0500

    DSF9.2_ADVANCED: Advanced instructions to extend FinalLaunchSequence

---
+++ org.eclipse.cdt.example.framespy/src/org/eclipse/cdt/example/framespy/FrameSpyFinalLaunchSequence.java
@@ -0,0 +1,18 @@
+/*******************************************************************************
+ * Copyright (c) 2016 Ericsson
+ *
+ * All rights reserved. This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License v1.0 which
+ * accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+
+package org.eclipse.cdt.example.framespy;
+
+public class FrameSpyFinalLaunchSequence {
+	// Global TODO: Extend DSF-GDB last FinalLaunchSequence (i.e. FinalLaunchSequence_7_7)
+	//              and add a step that will call FrameSpyService.setVerbose(true);
+	//              This new step should be the first step in the FinalLaunchSequence
+}
+
+
