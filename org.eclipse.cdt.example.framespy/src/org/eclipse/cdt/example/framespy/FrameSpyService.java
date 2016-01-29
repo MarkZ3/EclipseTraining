59a4d40dbedbe53cb0b47fa0fd4726f22659a018
Marc Khouzam <marc.khouzam@ericsson.com>
2016-01-29 13:05:39 -0500

    DSF4_ADVANCED: Advanced instruction to create service

---
+++ org.eclipse.cdt.example.framespy/src/org/eclipse/cdt/example/framespy/FrameSpyService.java
@@ -0,0 +1,28 @@
+/*******************************************************************************
+ * Copyright (c) 2016 Ericsson
+ *
+ * All rights reserved. This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License v1.0 which
+ * accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+
+package org.eclipse.cdt.example.framespy;
+
+public class FrameSpyService {
+
+	// Global TODO: Make this class into a DSF service that provides 
+	//              a synchronous method
+	//                   getLocalTimeOfDayString();
+	//              which synchronously returns 
+	//                   new SimpleDateFormat("HH:mm:ss").format(Calendar.getInstance().getTime());
+	//              and an asynchronous method
+	//                   getTargetTimeOfDayString();
+	//              which should still use (for simplicity)
+	//                   new SimpleDateFormat("HH:mm:ss").format(Calendar.getInstance().getTime());
+
+	// TODO: Look at any existing DSF-GDB service to guide you.
+	
+	// TODO: Don't forget to register the service so it can be found
+	//       using a DsfServicesTracker	
+}
