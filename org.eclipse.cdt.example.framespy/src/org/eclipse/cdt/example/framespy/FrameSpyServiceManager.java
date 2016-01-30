963542164293a8b8853500c131932d48183fd6c5
Marc Khouzam <marc.khouzam@ericsson.com>
2016-01-30 14:21:31 -0500

    DSF5_ANSWERS: Instantiating new services through a service manager

--- org.eclipse.cdt.example.framespy/src/org/eclipse/cdt/example/framespy/FrameSpyServiceManager.java
+++ org.eclipse.cdt.example.framespy/src/org/eclipse/cdt/example/framespy/FrameSpyServiceManager.java
@@ -9,17 +9,18 @@
 
 package org.eclipse.cdt.example.framespy;
 
+import org.eclipse.cdt.dsf.concurrent.RequestMonitor;
+import org.eclipse.cdt.dsf.service.DsfSession;
+import org.eclipse.cdt.dsf.service.DsfSession.SessionStartedListener;
+
 /**
  * This singleton class that starts FrameSpyServices 
  * as DSF sessions are created.  The class does not
  * worry about shutting down the service as it counts
  * on DSF-GDB to do it automatically.
  */
-public class FrameSpyServiceManager {
+public class FrameSpyServiceManager implements SessionStartedListener {
 
-	// Global TODO: Instantiate and initialize a FrameSpyService class
-	//              each time a DSF session is started.
-	
 	private static final FrameSpyServiceManager fInstance = new FrameSpyServiceManager();
 	
 	// Private constructor for singleton
@@ -27,14 +28,20 @@ public class FrameSpyServiceManager {
 	}
 
 	public static void initialize() {
-		// TODO: Register with DsfSession.addSessionStartedListener
-		//       Have this class implement the correct interface to
-		//       be the listener for Session started calls.
+		DsfSession.addSessionStartedListener(fInstance);
 	}
 
 	public static void dispose() {
+		DsfSession.removeSessionStartedListener(fInstance);
 	}
 
-	// TODO: Implement sessionStarted() method inherited from SessionStartedListener
-	//       Instantiate and initialize FrameSpyService in this new method.
+	@Override
+	public void sessionStarted(DsfSession session) {
+		FrameSpyService service = new FrameSpyService(session);
+		service.initialize(new RequestMonitor(session.getExecutor(), null));
+		
+		// No real need to store the service reference ourselves.
+		// When the service registers, a reference for it will be kept
+		// by DSF
+	}
 }
