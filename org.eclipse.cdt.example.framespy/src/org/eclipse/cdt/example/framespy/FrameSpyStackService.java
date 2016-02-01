b2fa4ad4a47aa51c0d883cb108de3dc8fded448e
Marc Khouzam <marc.khouzam@ericsson.com>
2016-01-31 21:58:20 -0500

    DSF7.1_ANSWERS: Modifying Stack service

--- org.eclipse.cdt.example.framespy/src/org/eclipse/cdt/example/framespy/FrameSpyStackService.java
+++ org.eclipse.cdt.example.framespy/src/org/eclipse/cdt/example/framespy/FrameSpyStackService.java
@@ -9,35 +9,59 @@
 
 package org.eclipse.cdt.example.framespy;
 
-public class FrameSpyStackService {
+import org.eclipse.cdt.core.IAddress;
+import org.eclipse.cdt.dsf.concurrent.DataRequestMonitor;
+import org.eclipse.cdt.dsf.gdb.service.extensions.GDBStack_HEAD;
+import org.eclipse.cdt.dsf.service.DsfSession;
 
-	// Global TODO: Replace the "main" function name with "entry" 
+public class FrameSpyStackService extends GDBStack_HEAD {
+	public FrameSpyStackService(DsfSession session) {
+		super(session);
+	}
 	
-	// TODO: Have this class extend DSF-GDB's Stack service.
-	//       You can find what is the name of the service that implements IStack
-	//       in DSF-GDB and extend that class.  Press F4 on IStack.
-	//       It is recommended to use the *_HEAD classes if you only care
-	//       about supporting the latest GDB version.
+	@Override
+	public void getFrameData(IFrameDMContext frameDmc, DataRequestMonitor<IFrameDMData> rm) {
+		super.getFrameData(frameDmc, new DataRequestMonitor<IFrameDMData>(getExecutor(), rm) {
+			@Override
+			protected void handleSuccess() {
+				IFrameDMData baseData = getData();
+				
+				rm.done(new IFrameDMData() {
+					@Override
+					public String getModule() {
+						return baseData.getModule();
+					}
+					
+					@Override
+					public int getLine() {
+						return baseData.getLine();
+					}
+					
+					@Override
+					public String getFunction() {
+						if (baseData.getFunction().equals("main")) {
+							return "entry";
+						} else {
+							return baseData.getFunction();
+						}
+					}
+					
+					@Override
+					public String getFile() {
+						return baseData.getFile();
+					}
+					
+					@Override
+					public int getColumn() {
+						return baseData.getColumn();
+					}
 
-	// TODO: Override getFrameData() to return a different IFrameDMData,
-	//       in which any "main" method is called "entry"
-	//
-	//       You will first call super.getFrameData() and store the
-	//       returned IFrameDMData in a variable called baseFrame.
-	//       You first instinct may be to modify that result and replace
-	//       "main" with "entry".  That would be fine, except that
-	//       IFrameDMData does not provide any methods to modify it.
-	// 
-	//       Instead, you can define a *new* class that also implements IFrameDMData.
-	//       In each method you must implement, you can return the result
-	//       contained in the baseFrame object you created above.
-	//       
-	//       Finally, in your implementation of IFrameDMData#getFunction()
-	//       you will check if baseFrame.getFunction().equals("main") and 
-	//       if is, you will instead, return "entry".
-	
-	// TODO: Call rm.done(new YourFrameData);
-	
-	// Note that this part of the exercise cannot be tested just yet.  It needs
-	// more changes which we will do as follow-ups.
+					@Override
+					public IAddress getAddress() {
+						return baseData.getAddress();
+					}
+				});
+			}
+		});
+	}
 }
