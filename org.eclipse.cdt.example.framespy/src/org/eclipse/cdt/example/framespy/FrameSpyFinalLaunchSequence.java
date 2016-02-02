7738e67f0997fbd41496cff691c9321b5edd1d5d
Marc Khouzam <marc.khouzam@ericsson.com>
2016-02-02 16:35:22 -0500

    DSF9.2_START: Instructions to extend FinalLaunchSequence

--- org.eclipse.cdt.example.framespy/src/org/eclipse/cdt/example/framespy/FrameSpyFinalLaunchSequence.java
+++ org.eclipse.cdt.example.framespy/src/org/eclipse/cdt/example/framespy/FrameSpyFinalLaunchSequence.java
@@ -13,6 +13,29 @@ public class FrameSpyFinalLaunchSequence {
 	// Global TODO: Extend DSF-GDB last FinalLaunchSequence (i.e. FinalLaunchSequence_7_7)
 	//              and add a step that will call FrameSpyService.setVerbose(true);
 	//              This new step should be the first step in the FinalLaunchSequence
-}
 
+	// TODO: Have this class extend FinalLaunchSequence_7_7
+	//       and create appropriate constructor.
+	
+	// TODO: Create a new method called stepSetVerbose() by mimicking
+	//       the signature of FinalLaunchSequence_7_7#stepSetDPrinfStyle.
+	//       Don't forget the @Execute annotation
+	
+	// TODO: In stepSetVerbose(), get the FrameSpyService using a
+	//       DsfServicesTracker.
+	//       Call FrameSpyService#setVerbose to enable verbosity in GDB
+	
+	// TODO: When calling FrameSpyService#setVerbose, in the passed RequestMonitor
+	//       override handleComplete() instead of the usual handleSuccess();
+	//       in handleComplete() ignore any error and just call (parent)rm.done()
+	//       This is avoid preventing the debugger from starting if in the future
+	//       -gdb-set verbosity on would fail for any reason (removed by GDB for example)
+	
+	// TODO: Insert the new stepSetVerbose() step into the overall FinalLaunchSequence.
+	//       Override getExecutionOrder() in a similar way as is done in FinalLaunchSequence_7_7
+	//       Insert the new step early in the steps; you can probably make it the first step.
+	//       You can also follow the trail of FinalLaunchSequence classes to figure out what 
+	//       are the order of the steps, to make a more informed choice.
+	//       
+}
 
