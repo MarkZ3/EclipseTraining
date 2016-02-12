127f0945787b43c3db9f58f692b5135d789e0e14
Marc-Andre Laperle <malaperle@gmail.com>
2016-02-12 01:11:46 +0100

    Increment/decrement level of complexity

--- org.eclipse.cdt.example.codeanalysis/src/org/eclipse/cdt/example/codeanalysis/ComplexityChecker.java
+++ org.eclipse.cdt.example.codeanalysis/src/org/eclipse/cdt/example/codeanalysis/ComplexityChecker.java
@@ -2,12 +2,16 @@ package org.eclipse.cdt.example.codeanalysis;
 
 import org.eclipse.cdt.codan.core.cxx.model.AbstractIndexAstChecker;
 import org.eclipse.cdt.core.dom.ast.ASTVisitor;
+import org.eclipse.cdt.core.dom.ast.IASTForStatement;
+import org.eclipse.cdt.core.dom.ast.IASTIfStatement;
 import org.eclipse.cdt.core.dom.ast.IASTStatement;
 import org.eclipse.cdt.core.dom.ast.IASTTranslationUnit;
+import org.eclipse.cdt.core.dom.ast.IASTWhileStatement;
 
 public class ComplexityChecker extends AbstractIndexAstChecker {
 
 	private final class ComplexityVisitor extends ASTVisitor {
+		private int level = 0;
 		
 		{
 			shouldVisitStatements = true;
@@ -15,13 +19,21 @@ public class ComplexityChecker extends AbstractIndexAstChecker {
 		
 		@Override
 		public int visit(IASTStatement statement) {
-			// TODO: increment complexity level
+			if (isStatementIncreasingComplexity(statement)) {
+				level++;
+			}
 			return super.visit(statement);
+		}
+
+		private boolean isStatementIncreasingComplexity(IASTStatement statement) {
+			return statement instanceof IASTIfStatement || statement instanceof IASTForStatement || statement instanceof IASTWhileStatement;
 		}
 		
 		@Override
 		public int leave(IASTStatement statement) {
-			// TODO: decrement complexity level
+			if (isStatementIncreasingComplexity(statement)) {
+				level--;
+			}
 			return super.leave(statement);
 		}
 	}
