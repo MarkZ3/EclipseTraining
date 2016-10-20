24068508799e2b87081a0b79e1384b3183e62e49
Bernd Hufmann <Bernd.Hufmann@ericsson.com>
2016-10-20 15:51:16 -0400

    Create Trace Compass example plug-in structure
    Signed-off-by: Bernd Hufmann <Bernd.Hufmann@ericsson.com>

---
+++ org.eclipse.tracecompass.example/src/org/eclipse/tracecompass/example/Activator.java
@@ -0,0 +1,60 @@
+/*******************************************************************************
+ * Copyright (c) 2016 Ericsson
+ *
+ * All rights reserved. This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License v1.0 which
+ * accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+
+package org.eclipse.tracecompass.example;
+
+import org.eclipse.ui.plugin.AbstractUIPlugin;
+import org.osgi.framework.BundleContext;
+
+/**
+ * The activator class controls the plug-in life cycle
+ */
+public class Activator extends AbstractUIPlugin {
+
+    /** The plug-in ID */
+    public static final String PLUGIN_ID = "org.eclipse.tracecompass.example"; //$NON-NLS-1$
+
+    // The shared instance
+    private static Activator plugin;
+    private static BundleContext bundleContext;
+
+    /**
+     * Constructor
+     */
+    public Activator() {
+    }
+
+    @Override
+    public void start(BundleContext context) throws Exception {
+        super.start(context);
+        plugin = this;
+        bundleContext = context;
+    }
+
+    @Override
+    public void stop(BundleContext context) throws Exception {
+        plugin = null;
+        super.stop(context);
+        bundleContext = null;
+    }
+
+    /**
+     * @return the shared instance
+     */
+    public static Activator getDefault() {
+        return plugin;
+    }
+
+    /**
+     * @return the bundle context
+     */
+    public static BundleContext getBundleContext() {
+        return bundleContext;
+    }
+}
