e958eff514d7835aadc726ec4fa9b99cefbd4683
Bernd Hufmann <Bernd.Hufmann@ericsson.com>
2016-10-21 06:58:45 -0400

    TRACECOMPASS3.3_ANSWERS: Add an empty view as analysis output
    Signed-off-by: Bernd Hufmann <Bernd.Hufmann@ericsson.com>

--- org.eclipse.tracecompass.training.example/src/org/eclipse/tracecompass/training/example/ProcessingStatesView.java
+++ org.eclipse.tracecompass.training.example/src/org/eclipse/tracecompass/training/example/ProcessingStatesView.java
@@ -18,31 +18,9 @@ public class ProcessingStatesView extends ViewPart {
 
     @Override
     public void createPartControl(Composite parent) {
-        /*
-         *  TODO:
-         *  - Update plugin.xml with manifest editor
-         *  - Goto to Extension tab
-         *  - click on "Add"
-         *      - select org.eclipse.ui.views and click Ok
-         *   - right mouse click on org.eclipse.ui.views -> New -> view
-         *      - fill-in:
-         *        id = org.eclipse.tracecompass.training.example.processing.states
-         *        name = Processing States
-         *        click "Browse..." and find ProcessinStatesView
-         *
-         *  - Select org.eclipse.linuxtools.tmf.core.analysis and right mouse click-> New -> output
-         *  - fill-in:
-         *      - class = org.eclipse.tracecompass.tmf.ui.analysis.TmfAnalysisViewOutput
-         *      - id = org.eclipse.tracecompass.training.example.processing.states
-         *  - Select TmfAnalysisViewOutput and right mouse click -> New -> analysisModuleClass
-         *  - Click "Browse..."
-         *  - Find ProcessingTimeAnalysis
-         *  - click on OK
-         */
     }
 
     @Override
     public void setFocus() {
     }
-
 }
