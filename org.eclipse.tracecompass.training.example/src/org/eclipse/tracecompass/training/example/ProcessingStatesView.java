327d76bed75c39f1e62af4a2b28b9af30229e2cc
Bernd Hufmann <Bernd.Hufmann@ericsson.com>
2016-10-27 15:38:41 -0400

    TRACECOMPASS4.4_ANSWERS: Perform state system queries and print results
    Signed-off-by: Bernd Hufmann <Bernd.Hufmann@ericsson.com>

--- org.eclipse.tracecompass.training.example/src/org/eclipse/tracecompass/training/example/ProcessingStatesView.java
+++ org.eclipse.tracecompass.training.example/src/org/eclipse/tracecompass/training/example/ProcessingStatesView.java
@@ -8,6 +8,8 @@
  *******************************************************************************/
 package org.eclipse.tracecompass.training.example;
 
+import java.util.List;
+
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
@@ -15,6 +17,11 @@ import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Text;
 import org.eclipse.tracecompass.statesystem.core.ITmfStateSystem;
+import org.eclipse.tracecompass.statesystem.core.StateSystemUtils;
+import org.eclipse.tracecompass.statesystem.core.exceptions.AttributeNotFoundException;
+import org.eclipse.tracecompass.statesystem.core.exceptions.StateSystemDisposedException;
+import org.eclipse.tracecompass.statesystem.core.interval.ITmfStateInterval;
+import org.eclipse.tracecompass.statesystem.core.statevalue.ITmfStateValue;
 import org.eclipse.tracecompass.tmf.core.signal.TmfSelectionRangeUpdatedSignal;
 import org.eclipse.tracecompass.tmf.core.signal.TmfSignalHandler;
 import org.eclipse.tracecompass.tmf.core.signal.TmfTraceSelectedSignal;
@@ -24,7 +31,6 @@ import org.eclipse.tracecompass.tmf.core.trace.TmfTraceManager;
 import org.eclipse.tracecompass.tmf.ui.views.TmfView;
 
 public class ProcessingStatesView extends TmfView {
-
     private Text fTextArea;
 
     /** The selected trace */
@@ -102,45 +108,76 @@ public class ProcessingStatesView extends TmfView {
         // make sure that it is fully build
         ssq.waitUntilBuilt();
         outputText("startTime=" + startTime + ", endTime=" + endTime);
+
         /*
          * 1) get the quarks of all children of attribute "Requester".
          *    Print the quark and attribute name to screen
          */
-        outputText("Exercise 1");
-        // TODO
-        outputText("--------------------------------------");
-        /*
-         * 2) Get ITmfStateInterval of attribute Requester/<requester> at startTime (single query)
-         *    Print value of ITmfStateValue
-         *    Question: what data type is the state value?
-         */
-        outputText("Exercise 2");
-        // TODO
-        outputText("--------------------------------------");
-        /*
-         *  3) Print ITmfStateValue of attribute Requester/<requester> at endTime
-         */
-        outputText("Exercise 3");
-        // TODO
-        outputText("--------------------------------------");
-        /*
-         * 4) Query history range using utility class StateSystemUtils and
-         *     print each ITmfStateInterval (state value, start time, end time and duration)
-         */
-        outputText("Exercise 4");
-        // TODO
-        outputText("--------------------------------------");
-        /*
-         * 5) Do a full query at end time and print each attribute, path and state interval
-         */
-        outputText("Exercise 5");
-        // TODO
-        outputText("--------------------------------------");
-        /*
-         * 6) Bonus: do step 4 with all attribute with path Requester/<requester>/<id>
-         */
-        outputText("Exercise 6");
-        // TODO
+        List<Integer> requesterQuarks = ssq.getQuarks("Requester", "*");
+        for (Integer requesterQuark : requesterQuarks) {
+            String requesterName = ssq.getAttributeName(requesterQuark);
+            outputText("Exercise 1");
+            outputText("attribute=" + requesterName + ", quark=" + requesterQuark);
+            outputText("--------------------------------------");
+            try {
+                /*
+                 * 2) Get ITmfStateInterval of attribute Requester/<requester> at startTime (single query)
+                 *    Print value of ITmfStateValue
+                 *    Question: what data type is the state value?
+                 */
+                outputText("Exercise 2");
+                ITmfStateInterval interval = ssq.querySingleState(startTime, requesterQuark);
+                ITmfStateValue state = interval.getStateValue();
+                outputText("state at start=" + state.toString());
+                outputText("--------------------------------------");
+                /*
+                 *  3) Print ITmfStateValue of attribute Requester/<requester> at endTime
+                 */
+                outputText("Exercise 3");
+                interval = ssq.querySingleState(endTime, requesterQuark);
+                state = interval.getStateValue();
+                outputText("state at end=" + state.toString());
+                outputText("--------------------------------------");
+                /*
+                 * 4) Query history range using utility class StateSystemUtils and
+                 *     print each ITmfStateInterval (state value, start time, end time and duration)
+                 */
+                outputText("Exercise 4");
+                List<ITmfStateInterval> intervals = StateSystemUtils.queryHistoryRange(ssq, requesterQuark, startTime, endTime);
+                for (ITmfStateInterval inter : intervals) {
+                    outputText("interval" + inter.toString());
+                }
+                outputText("--------------------------------------");
+                /*
+                 * 5) Do a full query at end time and print each attribute, path and state interval
+                 */
+                outputText("Exercise 5");
+
+                intervals = ssq.queryFullState(endTime);
+                for (ITmfStateInterval inter : intervals) {
+                    outputText("interval" + inter.toString() +
+                            ", \tattributeName=" + ssq.getAttributeName(inter.getAttribute()) +
+                            ", \tattributePath=" + ssq.getFullAttributePath(inter.getAttribute()));
+
+                }
+                outputText("--------------------------------------");
+                /*
+                 * 6) Bonus: do step 4 with all attribute with path Requester/<requester>/<id>
+                 */
+                outputText("Exercise 6");
+                List<Integer> idQuarks = ssq.getQuarks(requesterQuark, "*");
+                for (Integer idQuark : idQuarks) {
+                    intervals = StateSystemUtils.queryHistoryRange(ssq, idQuark, startTime, endTime);
+                    for (ITmfStateInterval inter : intervals) {
+                        outputText("interval" + inter.toString());
+                    }
+                }
+
+            } catch (StateSystemDisposedException | AttributeNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
+
         outputText("=========================================");
     }
     private void outputText(String text) {
