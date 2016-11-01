55d5912909391e77f9e1a155803f5a06f34520a3
Marc-Andre Laperle <malaperle@gmail.com>
2016-11-01 10:04:56 -0400

    TRACECOMPASS2.2_ANSWERS: Event request added

--- org.eclipse.tracecompass.training.example/src/org/eclipse/tracecompass/training/example/EventReader.java
+++ org.eclipse.tracecompass.training.example/src/org/eclipse/tracecompass/training/example/EventReader.java
@@ -1,8 +1,13 @@
 package org.eclipse.tracecompass.training.example;
 
+import org.eclipse.tracecompass.tmf.core.event.ITmfEvent;
+import org.eclipse.tracecompass.tmf.core.event.TmfEvent;
+import org.eclipse.tracecompass.tmf.core.request.ITmfEventRequest;
+import org.eclipse.tracecompass.tmf.core.request.TmfEventRequest;
 import org.eclipse.tracecompass.tmf.core.signal.TmfSignalHandler;
 import org.eclipse.tracecompass.tmf.core.signal.TmfSignalManager;
 import org.eclipse.tracecompass.tmf.core.signal.TmfTraceOpenedSignal;
+import org.eclipse.tracecompass.tmf.core.trace.ITmfTrace;
 
 public class EventReader {
 
@@ -12,8 +17,24 @@ public class EventReader {
 
     @TmfSignalHandler
     public void traceOpened(TmfTraceOpenedSignal signal) {
-        // TODO: Create a TmfEventRequest here.
-        // Output each event to the console.
-        // Output something when request is completed.
+        ITmfTrace trace = signal.getTrace();
+        if (trace != null) {
+            trace.sendRequest(new TmfEventRequest(TmfEvent.class,
+                    0, ITmfEventRequest.ALL_DATA,
+                    ITmfEventRequest.ExecutionType.BACKGROUND) {
+
+                @Override
+                public void handleData(ITmfEvent event) {
+                    super.handleData(event);
+                    System.out.println(event.getTimestamp());
+                }
+
+                @Override
+                public void handleCompleted() {
+                    super.handleCompleted();
+                    System.out.println("completed!");
+                }
+            });
+        }
     }
 }
